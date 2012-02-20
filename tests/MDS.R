3bb5d38fabe049576b35831921f240db9c464c5d
George Owusu <gowusu@gmail.com>
2012-02-20 00:00:00 +0000

    version 1.3

--- tests/MDS.R
+++ tests/MDS.R
@@ -3,7 +3,6 @@ library(qrfactor)
 data(UScereal, package="MASS")
 variables=c("calories","protein","sodium","carbo","sugars","potassium")
 data=UScereal[variables]
-
 #Multi dimensional scaling can be simulated as: 
 plot(qrfactor(data,scale='n'),plot="r",type="mds",factors=c(1,2))
 plot(qrfactor(data,scale='n'),plot="all",type="mds",factors=c(3,2))
